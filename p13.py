33ca8094e5ba0d6c5a754d1e1ae2c035e8d981f0
Tim Rijavec <tim@coder.si>
2017-12-15 18:33:50 +0000

    13

--- p13.py
+++ p13.py
@@ -1,10 +1,147 @@
-from common import run
+from common import run, read_lines
+import copy
 
+# 0: 3
+# 1: 2
+# 4: 4
+# 6: 4
 def p1():
-    pass
+    data = {}
+    state = {}
+    direction = {}
+    for line in read_lines(13):
+        idx, depth = line.split(': ')
+        data[int(idx)] = int(depth)
+        state[int(idx)] = 0
+        direction[int(idx)] = 1
+    keys = data.keys()
+    m = max(keys)
+    for x in range(m + 1):
+        if x not in data:
+            data[x] = 0
+            state[x] = 0
+    idx = -1
+    severity = []
+
+    # print data  
+    for x in range(m + 1):
+        idx += 1
+        # print idx, state
+        if data[x] > 0 and state[x] == 0:
+            severity.append(x)
+        for d,r in state.items():
+            if data[d] > 0:
+                state[d] += (1 * direction[d])
+                if state[d] >= data[d]:
+                    direction[d] = -1
+                    state[d] -=2
+                if state[d] < 0:
+                    direction[d] = 1
+                    state[d] += 2
+    print sum([d*x for d,x in data.items() if d in severity])
+
+import time
+
 
 def p2():
-    pass
+
+    # START = time.time()
+
+    # data = open("data/p13.txt", "r")
+    # rows = data.read().strip().split("\n")
+
+    # valDict = dict()
+
+    # for row in rows:
+    #     rowS = row.split(" ")
+    #     valDict[int(rowS[0][:-1])] = int(rowS[-1])
+
+    # caught = False
+    # for delay in xrange(10, 10**7):
+    #     caught = False
+    #     print 'DELAY', delay
+    #     for i in valDict.keys():
+    #         print i, (i+delay), (2* valDict[i] - 2), (i+delay) % (2* valDict[i] - 2) == 0
+    #         if (i+delay) % (2* valDict[i] - 2) == 0:
+    #             caught = True
+    #             break
+    #     if not caught:
+    #         print delay
+    #         break
+
+
+    # print "Time Taken:", time.time() - START
+
+    # 3946838
+
+    data = {}
+    state = {}
+    direction = {}
+    for line in read_lines(13):
+        idx, depth = line.split(': ')
+        data[int(idx)] = int(depth)
+        state[int(idx)] = 0
+        direction[int(idx)] = 1
+    keys = data.keys()
+    m = max(keys)
+    for x in range(m + 1):
+        if x not in data:
+            data[x] = 0
+            state[x] = 0
+
+    initial_data = copy.deepcopy(data)
+    initial_state = copy.deepcopy(state)
+    initial_direction = copy.deepcopy(direction)
+
+    for i in range(10):
+        for d in initial_state.keys():
+            if initial_data[d] > 0:
+                initial_state[d] += (1 * initial_direction[d])
+                if initial_state[d] >= initial_data[d]:
+                    initial_direction[d] = -1
+                    initial_state[d] -=2
+                if initial_state[d] < 0:
+                    initial_direction[d] = 1
+                    initial_state[d] += 2
+
+    for p in xrange(11, 10**7):
+        for d in initial_state.keys():
+            if initial_data[d] > 0:
+                initial_state[d] += (1 * initial_direction[d])
+                if initial_state[d] >= initial_data[d]:
+                    initial_direction[d] = -1
+                    initial_state[d] -=2
+                if initial_state[d] < 0:
+                    initial_direction[d] = 1
+                    initial_state[d] += 2
+
+        data = copy.deepcopy(initial_data)
+        state = copy.deepcopy(initial_state)
+        direction = copy.deepcopy(initial_direction)
+        idx = -1
+        severity = []
+
+        if p % 100 == 0:
+            print p
+
+        caught = False
+        for x in xrange(m + 1):
+            idx += 1
+            if data[x] > 0 and state[x] == 0:
+                caught = True
+                break
+            for d in state.keys():
+                if data[d] > 0:
+                    state[d] += (1 * direction[d])
+                    if state[d] >= data[d]:
+                        direction[d] = -1
+                        state[d] -=2
+                    if state[d] < 0:
+                        direction[d] = 1
+                        state[d] += 2
+        if not caught:
+            break
+    print p
 
 if __name__ == '__main__':
     run(p1, p2)
--- p14.py
+++ p14.py
@@ -1,10 +1,92 @@
 from common import run
 
+def do_hash(val):
+    lengths = []
+    sparse_hash = range(256)
+    for x in val:
+        lengths.append(ord(x))
+    lengths += [17,31,73,47,23]
+    
+    skip = 0
+    pos = 0
+    l = len(sparse_hash)
+    last = l - 1
+    for i in range(64):
+        for length in lengths:
+            overflow = 0
+            end = pos + length
+            if end > last:
+                overflow = end - l
+            selection = sparse_hash[pos:end]
+            if overflow:
+                selection += sparse_hash[:overflow]
+            p = 0
+            selection = selection[::-1]
+            for x in range(pos, min(l, end)):
+                sparse_hash[x] = selection[p]
+                p += 1
+            if overflow:
+                for x in range(overflow):
+                    sparse_hash[x] = selection[p]
+                    p += 1
+            pos += (length + skip)
+            while pos > last:
+                pos = pos - l
+            skip += 1
+    dense_hash = []
+    for x in range(16):
+        tmp = reduce(lambda i, j: int(i) ^ int(j), sparse_hash[(x*16):((x+1)*16)])
+        dense_hash.append(tmp)
+    hash_hexadecimal = ''.join('0x{:02x}'.format(x)[2:] for x in dense_hash)
+    return hash_hexadecimal
+
+def hex_to_bin(h):
+    scale = 16
+    num_of_bits = 4
+    data = []
+    for x in h:
+        data.append(bin(int(x, scale))[2:].zfill(num_of_bits))
+    return ''.join(data)
+
 def p1():
-    pass
+    # val = 'flqrgnkx-%d' # Test
+    val = 'amgozmfv-%d' # Actual
+    data = [hex_to_bin(do_hash(val % d)) for d in range(128)]
+    # for d in data:
+    #    print d
+    print (''.join(data)).count('1')
+
+def connected(regions, data, i, j, region=1):
+    if j < 0 or i < 0 or j >= len(data[0]) or i >= len(data):
+        return
+    if data[i][j] == '0':
+        return
+
+    data[i][j] = '0'
+    regions[i][j] = region
+    connected(regions, data, i+1, j, region)
+    connected(regions, data, i-1, j, region)
+    connected(regions, data, i, j+1, region)
+    connected(regions, data, i, j-1, region)
 
 def p2():
-    pass
+    # val = 'flqrgnkx-%d' # Test
+    val = 'amgozmfv-%d' # Actual
+    regions = []
+    data = [[a for a in hex_to_bin(do_hash(val % d))] for d in range(128)]
+    for a in xrange(128):
+        regions.append([])
+        for b in xrange(128):
+            regions[a].append(0)
+    region = 1
+    for a in xrange(128):
+        for b in xrange(128):
+            connected(regions, data, a, b, region)
+            region += 1
+
+    # for y in range(16):
+    #     print ['%4d' % x for x in regions[y][:16]]
+    print len(set([x for x in reduce(lambda x, y: x+y, regions) if x > 0]))
 
 if __name__ == '__main__':
     run(p1, p2)
