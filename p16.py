140758185e81f483569a67953abc8b0eeff2e47b
Tim Rijavec <tim@coder.si>
2017-12-17 09:28:07 +0000

    p16, p17

--- p16.py
+++ p16.py
@@ -1,10 +1,57 @@
-from common import run
+from common import run, read_lines_array
+
+def swap(text, f, t):
+    l = list(text)
+    tmp = l[f]
+    l[f] = l[t]
+    l[t] = tmp
+    return ''.join(l)
 
 def p1():
-    pass
+    for x in read_lines_array(16, ','):
+        operations = x
+    data = 'abcdefghijklmnop'
+    for op in operations:
+        if op[0] == 's':
+            s = int(op[1:])
+            data = data[-s:] + data[:-s]
+        elif op[0] == 'x':
+            f,t = op[1:].split('/')
+            f,t = int(f), int(t)
+            data = swap(data, f, t)
+
+        elif op[0] == 'p':
+            fn,tn = op[1:].split('/')
+            f = data.index(fn)
+            t = data.index(tn)
+            data = swap(data, f, t)
+    print data
 
 def p2():
-    pass
+    seen = []
+    for x in read_lines_array(16, ','):
+        operations = x
+    data = 'abcdefghijklmnop'
+    for i in xrange(1000000000):
+        if data in seen: 
+            print(seen[1000000000 % i])
+            break
+        seen.append(data)
+
+        for op in operations:
+            if op[0] == 's':
+                s = int(op[1:])
+                data = data[-s:] + data[:-s]
+            elif op[0] == 'x':
+                f,t = op[1:].split('/')
+                f,t = int(f), int(t)
+                data = swap(data, f, t)
+
+            elif op[0] == 'p':
+                fn,tn = op[1:].split('/')
+                f = data.index(fn)
+                t = data.index(tn)
+                data = swap(data, f, t)
 
 if __name__ == '__main__':
     run(p1, p2)
--- p17.py
+++ p17.py
@@ -1,10 +1,47 @@
 from common import run
+from collections import deque
 
 def p1():
-    pass
+    steps = 363
+    data = deque([0])
+    for x in xrange(1, 2018):
+        data.rotate(-steps)
+        data.append(x)
+    l = list(data)
+    index = l.index(2017) + 1
+    if index >= len(l):
+        index = 0
+
+    print l[index]
+
+    # Slow
+    # steps = 363
+    # data = [0]
+    # position = 0
+    # for x in xrange(1, 2018):
+    #     tmp = steps
+    #     position = (position + steps) % len(data)
+    #     data.insert(position + 1, x)
+    #     position += 1
+    #     if position >= len(data):
+    #         position = 0
+    # index = data.index(2017)
+    # print data[index + 1]
 
 def p2():
-    pass
+    steps = 363
+    data = deque([0])
+    for x in xrange(1, 50000001):
+        if x % 100000 == 0:
+            print x
+        data.rotate(-steps)
+        data.append(x)
+    l = list(data)
+    index = l.index(0) + 1
+    if index >= len(l):
+        index = 0
+
+    print l[index]
 
 if __name__ == '__main__':
     run(p1, p2)
